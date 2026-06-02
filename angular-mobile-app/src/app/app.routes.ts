456557c699f911e3b9d50da84e30071718be24ae
ariadnaholodenko <golodenkoariadna@gmail.com>
2026-06-02 16:51:40 +0200

    refactor: deleted page logic

--- angular-mobile-app/src/app/app.routes.ts
+++ angular-mobile-app/src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { WelcomePage } from './pages/welcome-page/welcome-page';
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
 import { ChatPage } from './pages/chat-page/chat-page';
-import { UnreadChatsPage } from './pages/unread-chats-page/unread-chats-page';
-import { FavoriteChatsPage } from './pages/favorite-chats-page/favorite-chats-page';
 import { InvitationsPage } from './pages/invitations-page/invitations-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
 import { authGuard } from './guards/auth.guard';
@@ -14,9 +12,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: 'chats', component: ChatPage, canActivate: [authGuard] },
-  { path: 'unread-chats', component: UnreadChatsPage, canActivate: [authGuard] },
-  { path: 'unread-chats', component: UnreadChatsPage, canActivate: [authGuard] },
-  { path: 'favorite-chats', component: FavoriteChatsPage, canActivate: [authGuard] },
   { path: 'invitations', component: InvitationsPage, canActivate: [authGuard] },
   { path: 'profile', component: ProfilePage, canActivate: [authGuard] },
   { path: '**', redirectTo: '' },
